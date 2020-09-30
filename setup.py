a5bf3ed95219e2f7e524b10fa4a3eb6a60b0df9d
wwilla7 <liangyuewilla@gmail.com>
2020-09-30 14:41:34 -0700

    adding instructions.

--- setup.py
+++ setup.py
@@ -38,5 +38,6 @@ setup(
 
     # Manual control if final package is compressible or not, set False to prevent the .egg from being made
     # zip_safe=False,
-    extras_require = {"TensorFlow-support": "tensorflow-gpu"},
+    extras_require = {"TF-support": "tensorflow-gpu"}, # Required for faster matrices solutions with GPUs
+
 )
